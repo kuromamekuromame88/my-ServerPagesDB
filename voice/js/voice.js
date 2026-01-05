40c6e29dd934d6034b837f09c301d4578cc68b62
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-05 15:39:31 +0900

    a

--- voice/js/voice.js
+++ voice/js/voice.js
@@ -7,11 +7,10 @@ import {
 
 import {
   SkyWayAuthToken
-} from "https://cdn.jsdelivr.net/npm/@skyway-sdk/token@1.x/dist/index.js";
+} from "https://cdn.jsdelivr.net/npm/@skyway-sdk/token@2.0.2/dist/index.js";
 
 import {
-  SkyWayRoom/*,
-  SkyWayAuthToken*/
+  SkyWayRoom
 } from "https://cdn.jsdelivr.net/npm/@skyway-sdk/room@2.2.1/dist/index.js";
 
 
