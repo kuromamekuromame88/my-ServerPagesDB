a76d7ae483ef9f2cd7eb93037dde50db90f30d63
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-24 13:58:48 +0900

    a

---
+++ webcreate/js/main.js
@@ -0,0 +1,11 @@
+  const editor = CodeMirror.fromTextArea(
+    document.getElementById("codeEditor"),
+    {
+      mode: "htmlmixed",
+      lineNumbers: true,
+      tabSize: 2,
+      indentUnit: 2,
+      autoCloseTags: true,
+      matchBrackets: true
+    }
+  );
