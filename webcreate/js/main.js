d3e35da969a8170c1753a506c6f09e4f6ffc7a24
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-25 14:21:14 +0900

    a

--- webcreate/js/main.js
+++ webcreate/js/main.js
@@ -1,26 +1,38 @@
-
 let editor;
 
 function preview() {
   document.getElementById("previewFrame").srcdoc = editor.getValue();
 }
 
-window.addEventListener("load", () => {
-  editor = CodeMirror.fromTextArea(
-    document.getElementById("mirror"),
-    {
-      mode: "htmlmixed",
-      lineNumbers: true,
-      tabSize: 2,
-      indentUnit: 2,
-      autoCloseTags: true,
-      matchBrackets: true,
-      autoCloseBrackets: true
+function showPage() {
+  previewFrame.style.opacity = 1;
+  previewFrame.style.pointerEvents = "auto";
+  EditFrame.style.opacity = 0;
+  EditFrame.style.pointerEvents = "none";
+}
 
-    }
-  );
+function hidePage() {
+  previewFrame.style.opacity = 0;
+  previewFrame.style.pointerEvents = "none";
+  EditFrame.style.opacity = 1;
+  EditFrame.style.pointerEvents = "auto";
+}
+
+window.addEventListener("load", () => {
+  editor = CodeMirror.fromTextArea(mirror, {
+    mode: "htmlmixed",
+    lineNumbers: true,
+    tabSize: 2,
+    indentUnit: 2,
+    autoCloseTags: true,
+    matchBrackets: true,
+    autoCloseBrackets: true
+  });
 
   editor.on("change", preview);
-
   preview();
+  showPage();
+
+  TrueViewBtn.onclick = showPage;
+  UIEditBtn.onclick = hidePage;
 });
