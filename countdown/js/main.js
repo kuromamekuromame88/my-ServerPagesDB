5fe19ef7f9b8c45ca8f1a68c1d26e0605f69ad6c
kuromamekuromame88 <tei8sia@outlook.jp>
2026-01-26 17:47:15 +0900

    a

--- countdown/js/main.js
+++ countdown/js/main.js
@@ -22,9 +22,6 @@ const photoStages = [
   { count: 320, url: "https://tool-webs.onrender.com/countdown/img/320.jpg" },
   { count: 640, url: "https://tool-webs.onrender.com/countdown/img/640.jpg" },
   { count: 1280, url: "https://tool-webs.onrender.com/countdown/img/1280.jpg" },
-  { count: 2560, url: "https://tool-webs.onrender.com/countdown/img/2560.jpg" },
-  { count: 5120, url: "https://tool-webs.onrender.com/countdown/img/5120.jpg" },
-  { count: 10240, url: "https://tool-webs.onrender.com/countdown/img/10240.jpg" },
 ];
 
 // ===== 日数 =====
