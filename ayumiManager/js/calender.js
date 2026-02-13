ae2e41a903bb16b3b5337a097904e59f5ef374c9
kuromamekuromame88 <tei8sia@outlook.jp>
2026-02-13 18:06:17 +0900

    a

--- ayumiManager/js/calender.js
+++ ayumiManager/js/calender.js
@@ -22,9 +22,10 @@ function next(){
 function patchEvent(c){
   const days = Array.from(c.getElementsByTagName("td"));
   days.forEach(e => {
-    console.log(!e.classList.contains("disabled"));
+    //console.log(!e.classList.contains("disabled"));
     if (!e.classList.contains("disabled")) {
       e.addEventListener("click", function () {
+        console.log("clicked!");
         if (this.dataset.day) {
           console.log(this.dataset.day);
         }
