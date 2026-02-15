107ffc99f6d030760dfe69c0e7f090af8f871f4c
kuromamekuromame88 <tei8sia@outlook.jp>
2026-02-15 16:19:05 +0900

    a

--- ayumiManager/js/main.js
+++ ayumiManager/js/main.js
@@ -1,7 +1,8 @@
 
 async function openSchedule(day){
   if(!day) return;
-  const d = day.replace(/-/g, "");
+  const sp = day.split("-");
+  const d = `${sp[0]}${sp[1]>9?sp[1]:"0"+sp[1]}${sp[2]>9?sp[2]:"0"+sp[2]}`;
   const s = await fetch(`https://tool-webs.onrender.com/ayumi?day=${d}`);
   const res = s.json();
   if(res.day != d) return;
