1c148ffd2685f6866d946de385d6176eee8bb7e5
kuromamekuromame88 <tei8sia@outlook.jp>
2026-02-15 17:03:23 +0900

    a

--- ayumiManager/js/main.js
+++ ayumiManager/js/main.js
@@ -20,8 +20,9 @@ async function openSchedule(day){
   console.log("s",subjects);
   subjects.forEach(e=>{
     var k = Object.keys(e)[0];
-    console.log("e[k]", e[k]);
+    //console.log("e[k]", e[k]);
     var kk = Object.keys(e[k])[0];
+    console.log(kk);
     dc(k, kk, e[k][kk]);
     alert("k",k);
     alert("kk",kk);
