23387d8e586e965ae423167157aff748be47eb0e
kuromamekuromame88 <tei8sia@outlook.jp>
2026-03-06 16:06:56 +0900

    a

--- DrawWay/js/main.js
+++ DrawWay/js/main.js
@@ -1,10 +1,8 @@
 //Matter.jsの読み込み
 const { Engine, Render, Runner, Bodies, World, Composite } = Matter;
 
-//エンジン作成
 const engine = Engine.create();
 
-//レンダラーの作成
 const render = Render.create({
   element: document.getElementById("game"),
   engine: engine,
@@ -14,23 +12,18 @@ const render = Render.create({
   },
 });
 
-//テスト用にオブジェクトの追加
 var boxA = Bodies.rectangle(window.innerWidth/2, window.innerHeight/2, 80, 80);
 
-//物体を世界に追加
 Composite.add(engine.world, [boxA]);
 
-//レンダラー内で実行
+
 Render.run(render);
-
-//レンダラーの作成
 var runner = Runner.create();
-
-//本体のエンジンを起動
 Runner.run(runner, engine);
 
-//画面リサイズに対応
+//画面リサイズ
 window.addEventListener("resize", ()=>{
   render.canvas.width = window.innerWidth;
   render.canvas.height = window.innerHeight;
 });
+
