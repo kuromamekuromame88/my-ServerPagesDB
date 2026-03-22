0c5ea43da1754fd94d71784936af1fe1ecdac0b0
kuromamekuromame88 <tei8sia@outlook.jp>
2026-03-22 19:26:09 +0900

    a

---
+++ chat/login/js/login.js
@@ -0,0 +1,22 @@
+
+async function login(){
+  const un=document.getElementById("user").value;
+  const pw=document.getElementById("pass").value;
+  const payload={
+    user: un,
+    pass: pw,
+  };
+  try{
+    const r=await fetch("/api/webchat/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    });
+    const result=await r.json();
+    if(result.ok) return;
+  }catch(e){
+    console.log(e);
+  }
+}
