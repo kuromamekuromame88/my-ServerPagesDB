ecb69b9c62fa4505c03303ef5432a2793fd03fb8
kuromamekuromame88 <tei8sia@outlook.jp>
2026-03-24 16:27:29 +0900

    a

--- chat/login/js/login.js
+++ chat/login/js/login.js
@@ -1,15 +1,8 @@
 
-
-document.querySelector(".Login").addEventListener("click", login);
-
 function showlog(e, isError){
   document.querySelector(".LoginMessage").innerText=e;
   document.querySelector(".LoginMessage").style.color=isError?"red":"green"; 
 }
-
-document.addEventListener("keydown", e=>{
- if(e.key=="Enter") login();
-});
 
 async function login(){
   const un=document.getElementById("user").value;
@@ -33,11 +26,36 @@ async function login(){
     const result=await r.json();
     showlog(result.state?"認証に成功しました！リダイレクトします...":"認証に失敗しました！", !result.state);
     if(!result.state) return;
+
+    //ログイン状態の保存
+    if(!localStorage.getItem("KEEPLOGIN")) localStorage.setItem("KEEPLOGIN", false);
+    if(document.getElementById("keeplogin").checked){
+      localStorage.setItem("KEEPLOGIN", true);
+    }else{
+      localStorage.setItem("KEEPLOGIN", false);
+    }
+
     const sid = result?.sid;
     setTimeout(()=>{
-      location.href=`https://tool-webs.onrender.com/chat?login=true&userID=${un}&sid=${sid}`
+      location.href=`https://tool-webs.onrender.com/chat?login=true&userID=${un}&sid=${sid}`;
     }, 1000);
   }catch(e){
     console.log(e);
   }
+}
+
+
+document.querySelector(".Login").addEventListener("click", login);
+
+
+
+document.addEventListener("keydown", e=>{
+ if(e.key=="Enter") login();
+});
+
+if(localStorage.getItem("KEEPLOGIN")=="true"){
+  showlog("ログインします...");
+  setTimeout(()=>{
+    location.href=`https://tool-webs.onrender.com/chat?login=true&keeplogin=true`;
+  }, 500);
 }
