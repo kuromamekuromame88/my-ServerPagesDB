e4824dd43949b02643841233fd1258acb82eb1d9
kuromamekuromame88 <tei8sia@outlook.jp>
2026-04-19 07:15:49 +0900

    a

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -814,7 +814,6 @@ function waitwscon(ws, timeoutMs = 10000) {
 // ------------------- UI表示 -------------------
 function showChatUI() {
   console.log("showChatUIが呼び出されました!")
-  usernameSetup.style.display = "none";
   chatUI.style.display = "block";
 }
 
